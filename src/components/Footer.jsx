c5b309bc2a349dec71fddea3767eb49f02a34baa
Ayush Singh <mr.technical19@gmail.com>
2023-04-25 12:01:42 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+
+const avatarSrc = "https://avatars.githubusercontent.com/u/62198074?v=4";
+
+const Footer = () => {
+  return (
+    <Box
+      bgColor={"blackAlpha.900"}
+      color={"whiteAlpha.700"}
+      minH={"48"}
+      px={"16"}
+      py={["16", "8"]}
+    >
+      <Stack direction={["column", "row"]} h={"full"} alignItems={"center"}>
+        <VStack w={"full"} alignItems={["center", "flex-start"]}>
+          <Text fontWeight={"bold"}>About Us</Text>
+          <Text
+            fontSize={"sm"}
+            letterSpacing={"widest"}
+            textAlign={["center", "left"]}
+          >
+            React App where users can View top cryptocurrency prices live, crypto charts, market cap, and trading volume.  
+          </Text>
+        </VStack>
+
+        <VStack>
+          <Avatar boxSize={"28"} mt={["4", "0"]} src={avatarSrc} />
+          <Text>Our Founder</Text>
+        </VStack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
